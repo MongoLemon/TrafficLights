88b642a09729dac26f4c970b095a7ec25cd07759
MongoLemon <xsuhust@gmail.com>
2015-08-04 11:44:56 -0700

    Add Route Class and its description. Use two thread to keep adding new vehicles and time tracking on lights.

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # TrafficLights
-Traffic Lights OO Design
 
-THis project is working on traffic lights simulation ! Have fun with it !
+@ Route Class: each Route object stands for one direction, 12 objects in total. 
+	-- In each Route, new moto are randomly added to and stored at the collection. 
+	-- Each Route will check the light every two seconds. If the lights is G, then remove the very first moto in the collection.
+	-- Every two seconds, each Route will check whether its light is G. If one G light turns to R, the light of next Route has to turn G.
+
+	-- 12 Routes in total. 4 right-turn lights are keeping G. The other 8 lights are grouped into 4 pairs. 
+	-- The 4 pairs of lights will turn G in a cycle round. In the Light Class, we need to mark the light of the opposite direction, as well as the next direction.
+	-- Cause the light of each direction has only one object in the whole system, the Light Class is better to be enum
+	
+  In each Route, we have name(direction), motos(vehicles storage collection). In the construction method, create one thread to add one new moto to the collection every random interval.
+  We also create a thread of timer to check whether the light of current Route is G. If YES, then remove the first one from the collection.
+  
---
+++ src/Route.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by anthony.su on 8/4/15.
+ */
+public class Route {
+
+    private List<String> motos = new ArrayList<String>();
+
+    private String name = null;
+
+    // in construction method, we create two threads
+    public Route(String name) {
+        this.name = name;
+
+        // create a thread pool to add moto to collection. Here is how cars keep driving on the road
+        ExecutorService pool = Executors.newSingleThreadExecutor();
+
+        // assign the task of adding new motos to the pool. it will execute a thread
+        pool.execute(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 1; i < 1000; i++) {
+                    try {
+                        Thread.sleep((new Random().nextInt(10) + 1) * 1000); //  1 - 10 sec random interval
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    motos.add(Route.this.name + "_" + i); // return external class field
+                }
+            }
+        });
+
+        // every two seconds, check on the light status. If it is G, remove one head moto
+        ScheduledExecutorService timer = Executors.newScheduledThreadPool(1);
+        timer.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                if (motos.size() > 0) {
+                    boolean GisOn = Lamp.valueOf(Route.this.name).isLighted();
+                    if (GisOn) {
+                        System.out.println(motos.remove(0) + " is passing the intersection ！");
+                    }
+                }
+            }
+        }, 1, 1, TimeUnit.SECONDS);
+    }
+}
